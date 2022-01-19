71a72ba7f5ad7b44b6960451b2825f0c3b4b25d6
John Ide <ide.johnc@gmail.com>
2022-01-19 02:39:08 -0500

    Removing timeTrack

--- todoist/utils.go
+++ todoist/utils.go
@@ -1,16 +1,5 @@
 package todoist
 
-import (
-	"log"
-	"time"
-)
-
-// timeTrack measures the execution time of a method
-func timeTrack(start time.Time, name string) {
-	elapsed := time.Since(start)
-	log.Printf("%s took %s", name, elapsed)
-}
-
 // p<Type> functions are used to convert basic types to their pointer equivalents
 func pString(s string) *string {
 	return &s
